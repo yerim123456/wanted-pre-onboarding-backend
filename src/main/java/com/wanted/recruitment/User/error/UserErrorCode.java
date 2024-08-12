2abd61049aff4e2e7b8b7138d15428aa39c39fb7
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-08-12 23:04:33 +0900

    feat: user error code 추가 (#4)

---
+++ src/main/java/com/wanted/recruitment/User/error/UserErrorCode.java
@@ -0,0 +1,23 @@
+package com.wanted.recruitment.User.error;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import com.wanted.recruitment.global.error.ErrorCode;
+
+@Getter
+public enum UserErrorCode implements ErrorCode {
+    AUTHORIZATION_FAILED(HttpStatus.UNAUTHORIZED, "인가에 실패했습니다."),
+    INVALID_USER_TYPE(HttpStatus.BAD_REQUEST, "해당하는 사용자 권한이 없습니다."),
+    CAN_NOT_ACCESS_TO_COMPANY_TYPE(HttpStatus.BAD_REQUEST, "사용자는 회사 권한에 접근할 수 없습니다."),
+    COMPANY_NOT_EXIST(HttpStatus.NOT_FOUND, "해당하는 회사가 없습니다."),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    UserErrorCode(HttpStatus httpStatus, String message) {
+        this.httpStatus = httpStatus;
+        this.message = message;
+    }
+}
