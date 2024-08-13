e06179cd290eb39797132db1374a7bbdf833122c
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-08-13 17:43:00 +0900

    test: 채용공고 생성 메소드 추가 (#6)

--- src/test/java/com/wanted/recruitment/setup/MockEntityFactory.java
+++ src/test/java/com/wanted/recruitment/setup/MockEntityFactory.java
@@ -1,5 +1,8 @@
 package com.wanted.recruitment.setup;
 
+import java.time.LocalDateTime;
+
+import com.wanted.recruitment.Employment.domain.entity.Employment;
 import com.wanted.recruitment.User.domain.entity.Company;
 import com.wanted.recruitment.User.domain.entity.User;
 
@@ -31,6 +34,18 @@ public class MockEntityFactory {
 			.email("yearim1226@naver.com")
 			.nickname("yerim")
 			.profileImagePath("https://img-url.com")
+			.build();
+	}
+
+	// Employment
+	public static Employment createMockEmployment(String companyName) {
+		return Employment.builder()
+			.company(createMockCompany(companyName))
+			.position("Backend Developer")
+			.content("Job Content")
+			.skill("Java")
+			.startDate(LocalDateTime.now())
+			.endDate(LocalDateTime.now().plusDays(10))
 			.build();
 	}
 }
