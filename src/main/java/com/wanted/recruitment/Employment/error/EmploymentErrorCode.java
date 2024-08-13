66222e921437ee80505f7f2456524663d6c883d1
yerim1ee <97941141+yerim123456@users.noreply.github.com>
2024-08-13 19:42:33 +0900

    feat: 채용공고 기간 관련 error code 추가 (#9)

--- src/main/java/com/wanted/recruitment/Employment/error/EmploymentErrorCode.java
+++ src/main/java/com/wanted/recruitment/Employment/error/EmploymentErrorCode.java
@@ -10,9 +10,11 @@ import lombok.Getter;
 public enum EmploymentErrorCode implements ErrorCode {
     EMPLOYMENT_NOT_EXIST(HttpStatus.NOT_FOUND, "해당하는 채용공고가 없습니다."),
     INVALID_ACCESS_TO_EMPLOYMENT(HttpStatus.BAD_REQUEST, "해당 회사가 작성한 채용공고가 아닙니다."),
+    INVALID_DATE_PERIOD(HttpStatus.BAD_REQUEST, "시작 일시는 마감 일시보다 이전이거나 같아야 합니다."),
     CAN_NOT_SAVE_SAME_EMPLOYMENT(HttpStatus.CONFLICT, "해당 회사, 직책의 채용공고가 존재하며, 아직 마감되지 않았습니다."),
     EMPLOYMENT_LIST_NOT_EXIST(HttpStatus.NOT_FOUND, "조회할 채용공고가 없습니다."),
     KEYWORD_EMPLOYMENT_LIST_NOT_EXIST(HttpStatus.NOT_FOUND, "해당 키워드에 해당하는 채용공고가 없습니다."),
+    INVALID_EMPLOYMENT_APPLY_PERIOD(HttpStatus.BAD_REQUEST, "해당 채용공고 지원 기간이 아닙니다."),
     ;
 
     private final HttpStatus httpStatus;
